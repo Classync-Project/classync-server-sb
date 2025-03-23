4d479c39616e488590079cc9e7dc1dc22386ec02
Kris0011 <krishp759@gmail.com>
2025-03-23 16:02:28 +0530

    Renamed 'server' folder to 'springboot_server'

--- src/main/java/com/classync/project/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.classync.project.repository;
-
-import com.classync.project.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name); 
-}
--- src/main/java/com/classync/project/services/UserClassroomService.java
+++
@@ -1,6 +0,0 @@
-package com.classync.project.services;
-
-
-public interface UserClassroomService {
-    public boolean updateUserRole(int userId, Long classroomId, String newRole);
-}
